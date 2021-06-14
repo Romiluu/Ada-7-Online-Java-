0dd1c6831f3db6d435ee3a822f366d70fec95066
Romiluu <romiiluna@hotmail.com>
2021-06-14 07:23:12 -0300

    Nuevos Ejercicios

---
+++ src/Ejercicios/Ejercicio999CajasCobranza.java
@@ -0,0 +1,42 @@
+package Ejercicios;
+
+/*Se tiene una linea de 4 cajas donde se cobran facturas que luego seran cargadas en el sistema. 
+ * Se solicitara el numero de caja y el importe cobrado.
+ *  La carga finaliza con importe cero. 
+ *  Informar la cantidad de facturas cobradas por cada caja y el promedio de todas las facturas
+ */
+import java.util.Scanner;
+
+public class Ejercicio999CajasCobranza {
+
+	public static void main(String[] args) {
+		int cantidadFacturasCaja[] = new int[4];
+		double promedio = 0;
+		int totalFacturas = 0;
+
+		System.out.println("SISTEMA DE CAJAS");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingrese importe factura (0=terminar)");
+		double importe = sc.nextDouble();
+		while (importe != 0) {
+			System.out.println("Ingrese caja de pago (1 a 4)");
+			int posicionCaja = sc.nextInt();
+			cantidadFacturasCaja[posicionCaja - 1]++;
+			totalFacturas++;
+			// promedio = promedio + importe;
+			promedio += importe;
+			sc = new Scanner(System.in);
+			System.out.println("Ingrese importe factura (0=terminar)");
+			importe = sc.nextDouble();
+		}
+
+		for (int i = 0; i < cantidadFacturasCaja.length; i++) {
+			System.out.println("Caja: " + (i + 1) + " --> " + cantidadFacturasCaja[i]);
+		}
+
+		System.out.println("Promedio: " + (promedio / totalFacturas));
+
+	}
+
+}
