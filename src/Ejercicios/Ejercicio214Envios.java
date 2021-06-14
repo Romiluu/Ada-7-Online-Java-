cbb102740fd0ccfb96071fad81b2d262f0fd011d
Romiluu <romiiluna@hotmail.com>
2021-06-14 01:03:29 -0300

    Ejercicio 214 envios

---
+++ src/Ejercicios/Ejercicio214Envios.java
@@ -0,0 +1,16 @@
+package Ejercicios;
+//En un trayecto entre dos ciudades se deben realizar paradas intermedias. 
+
+//Se desea conocer cuantas paradas
+//se van a realizar en el viaje y la longitud de los trayectos en kilometros. 
+//Se desea conocer la longitud
+//promedio entre ciudades y 
+//cuales son las paradas mas cercanas
+
+//ejemplo
+//
+//*------- * ---------------- * ----- * ----------- *
+//   40            100            15      60
+public class Ejercicio214Envios {
+
+}
